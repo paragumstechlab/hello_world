60411453a6424652714fea573c729f2a0177a1b6
Parag Jadhav <parag@umstechlab.com>
2016-05-05 16:02:53 +0530

    Come on work dammit

--- HashMapTest.java
+++ HashMapTest.java
@@ -21,29 +21,6 @@ public class HashMapTest
 		String formatedDate = cal.get(Calendar.DATE) + "/" + (cal.get(Calendar.MONTH) + 1) + "/" +         cal.get(Calendar.YEAR);
 		System.out.println("Formatted Date : " + formatedDate);
 
-		/*Map<Integer,Map<String,String>> user_map1 = new HashMap<Integer,Map<String,String>>();
-		Map<String,String> user_set1 = new HashMap<String,String>();
-
-		user_set1.put("user_name", "Parag");
-		user_set1.put("user_email", "abc@example.com");
-		user_map1.put(1, user_set1);
-
-		user_set1.put("user_name", "Parag1");
-		user_set1.put("user_email", "xyz@example.com");
-		user_map1.put(2, user_set1);
-
-		user_set1.put("user_name", "Parag2");
-		user_set1.put("user_email", "pqr@example.com");
-		user_map1.put(3, user_set1);
-
-	    //System.out.println(user_map1.get(1));
-
-
-		for (int itr : user_map1.keySet())
-		{
-			System.out.println(itr+" : "+user_map1.get(itr));
-		}*/
-
 		Map<Integer,Set<String>> user_map = new HashMap<Integer,Set<String>>();
 		Set<String> user_set = new HashSet<String>();
 
